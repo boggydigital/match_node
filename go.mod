8ee95ba84987d9c369ef214d22d732fb553c85d5
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-05-27 00:08:05 -0700

    Moving code from arelate/gog_auth to share with yt_urls

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/boggydigital/match-node
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20210525063256-abc453219eb5
---
+++ match_node.go
@@ -0,0 +1,20 @@
+package match_node
+
+import "golang.org/x/net/html"
+
+func matchNode(doc *html.Node, match func(node *html.Node) bool) *html.Node {
+	var f func(*html.Node) *html.Node
+	f = func(n *html.Node) *html.Node {
+		if match(n) {
+			return n
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			val := f(c)
+			if val != nil {
+				return val
+			}
+		}
+		return nil
+	}
+	return f(doc)
+}
