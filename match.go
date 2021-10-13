8151cf219be566790ec576c018258b81622874df
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-10-12 19:29:47 -0700

    Adding export delegate type

--- match.go
+++ match.go
@@ -2,10 +2,12 @@ package match_node
 
 import "golang.org/x/net/html"
 
-func Match(doc *html.Node, match func(node *html.Node) bool) *html.Node {
+type MatchDelegate func(node *html.Node) bool
+
+func Match(doc *html.Node, matchDelegate MatchDelegate) *html.Node {
 	var f func(*html.Node) *html.Node
 	f = func(n *html.Node) *html.Node {
-		if match(n) {
+		if matchDelegate(n) {
 			return n
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
