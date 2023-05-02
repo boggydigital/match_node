c422d20a48fbad919a678eb18ca6b8e0ff8ebdcc
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-05-01 20:36:41 -0700

    Adding a simple selector syntax tagName#id.class

---
+++ selector.go
@@ -0,0 +1,75 @@
+package match_node
+
+import (
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"strings"
+)
+
+const (
+	idPrefix    = "#"
+	classPrefix = "."
+)
+
+type selector struct {
+	et atom.Atom
+	id string
+	cl string
+}
+
+func NewSelector(query string) Matcher {
+	tag, id, class := "", "", ""
+
+	if strings.Contains(query, idPrefix) {
+		rest := ""
+		tag, rest, _ = strings.Cut(query, idPrefix)
+		if strings.Contains(rest, classPrefix) {
+			id, class, _ = strings.Cut(rest, classPrefix)
+		} else {
+			id = rest
+		}
+	} else if strings.Contains(query, classPrefix) {
+		tag, class, _ = strings.Cut(query, classPrefix)
+	} else {
+		tag = query
+	}
+
+	a := atom.Lookup([]byte(tag))
+
+	return &selector{
+		et: a,
+		id: id,
+		cl: class,
+	}
+}
+
+func (s *selector) Match(node *html.Node) bool {
+
+	if (s.et != 0 && node.DataAtom != s.et) ||
+		(s.id != "" && len(node.Attr) == 0) ||
+		(s.cl != "" && len(node.Attr) == 0) {
+		return false
+	}
+
+	if s.id != "" {
+		for _, attr := range node.Attr {
+			if attr.Key == "id" {
+				if attr.Val != s.id {
+					return false
+				}
+			}
+		}
+	}
+
+	if s.cl != "" {
+		for _, attr := range node.Attr {
+			if attr.Key == "class" {
+				if !strings.Contains(attr.Val, s.cl) {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
+}
